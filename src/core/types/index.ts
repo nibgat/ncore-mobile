f9bb7eaf6e7b1f78d4d0159a089553289d139bfb
lfabl <met2net3@gmail.com>
2022-04-04 02:19:30 +0300

    Bugfix: Type editing.

--- src/core/types/index.ts
+++ src/core/types/index.ts
@@ -79,7 +79,7 @@ declare global {
 
         type TypograpghyVariant = {
             fontFamily: string;
-            fontWeight: "normal" | "bold" | "100" | "200" | "300" | "400" | "500" | "600" | "700" | "800" | "900";
+            fontWeight?: "normal" | "bold" | "100" | "200" | "300" | "400" | "500" | "600" | "700" | "800" | "900";
             fontSize: number;
             lineHeight?: number;
             letterSpacing?: number;
